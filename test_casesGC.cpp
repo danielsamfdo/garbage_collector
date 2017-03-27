87c0947daa40c57f12e30eec03f937013acacaf4
Daniel Sam Pete Thiyagu <danielsamfdo@gmail.com>
2017-03-27 16:05:38 -0400

    Adding Minor Changes

--- test_casesGC.cpp
+++ test_casesGC.cpp
@@ -204,21 +204,21 @@ int main()
 
   cout << "-----------" << endl;
   cout << "----------- MAIN PROGAM -----------" << endl;
-  // testCaseGarbageOne();
-  cout<< "----------- TEST GLOBALS CASE START -----------"<<endl;
-  testGlobalsStillPresent();
-  Header *h = (Header *)malloc(sizeof(Header));
+  testCaseGarbageOne();
+  // cout<< "----------- TEST GLOBALS CASE START -----------"<<endl;
+  // testGlobalsStillPresent();
+  // Header *h = (Header *)malloc(sizeof(Header));
 
-  if(true){
-    // functionTwo(h);
-    char * t2 = (char *) malloc(maxNextGC);
-    char * testOther = (char *) malloc(maxNextGC);
-    // char * t3 = (char *) malloc(maxNextGC+1); //Trigger GC
-    cout<< "Address of T2 should be equal to Address of T4  :  :-)" << (size_t)t2 << endl;
-    // GC Would be triggered, now need to make sure the old one is getting added to freed objects;  
-  }
+  // if(true){
+  //   // functionTwo(h);
+  //   char * t2 = (char *) malloc(maxNextGC);
+  //   char * testOther = (char *) malloc(maxNextGC);
+  //   // char * t3 = (char *) malloc(maxNextGC+1); //Trigger GC
+  //   cout<< "Address of T2 should be equal to Address of T4  :  :-)" << (size_t)t2 << endl;
+  //   // GC Would be triggered, now need to make sure the old one is getting added to freed objects;  
+  // }
 
-  cout<< "----------- TEST GLOBALS CASE END -----------"<<endl;
+  // cout<< "----------- TEST GLOBALS CASE END -----------"<<endl;
   // testCaseLivenessOne();
   // testCaseLivenessTwo();
   // int ** p1 = (int **) malloc(8);
